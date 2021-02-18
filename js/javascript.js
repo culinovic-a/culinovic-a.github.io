f5dee9150688efb93993cdb85832dff8c72a38e2
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2021-02-18 10:04:21 +0100

    added two new projects, replaced the link for commit api call with the latest project

--- js/javascript.js
+++ js/javascript.js
@@ -7,32 +7,32 @@ $(document).ready(function () {
     $(".test").show()
   }, 6000)
 
-  // FORM VALIDATION
-  ;(function () {
-    "use strict"
-    window.addEventListener(
-      "load",
-      function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName("needs-validation")
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-          form.addEventListener(
-            "submit",
-            function (event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault()
-                event.stopPropagation()
-              }
-              form.classList.add("was-validated")
-            },
-            false
-          )
-        })
-      },
-      false
-    )
-  })()
+    // FORM VALIDATION
+    ; (function () {
+      "use strict"
+      window.addEventListener(
+        "load",
+        function () {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName("needs-validation")
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener(
+              "submit",
+              function (event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault()
+                  event.stopPropagation()
+                }
+                form.classList.add("was-validated")
+              },
+              false
+            )
+          })
+        },
+        false
+      )
+    })()
 
   // TYPING EFFECT
   function typeWriter(json, speed, sentenceIndex, letterIndex) {
@@ -73,7 +73,7 @@ $(document).ready(function () {
   var request = new XMLHttpRequest()
   request.open(
     "GET",
-    "https://api.github.com/repos/culinovic-a/Notes/commits/master",
+    "https://api.github.com/repos/culinovic-a/games-benchmarking/commits/main",
     true
   )
   request.onload = function () {
