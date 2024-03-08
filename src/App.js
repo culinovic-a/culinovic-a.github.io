3477f98e930d5fdb790449f7a60024e6536787e7
alex <aleksandra.c@badsistems.com>
2024-03-08 15:01:39 +0100

    change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1>Hey there! I'm giving my website a total makeover! In the meantime, if you need to reach out or connect, you can find me on <a target="_blank" href="https://www.linkedin.com/in/culinovic-a/">LinkedIn</a>.</h1>
+      <h1>Hey there! I'm giving my website a total makeover. In the meantime, if you need to reach out or connect, you can find me on <a target="_blank" href="https://www.linkedin.com/in/culinovic-a/">LinkedIn</a>.</h1>
     </div>
   );
 }
