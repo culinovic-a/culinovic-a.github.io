cba465cc0e18c1eb4bfe9c2e7858f1e1387bf0f6
alex <aleksandra.c@badsistems.com>
2024-03-08 13:25:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hey there! I'm giving my website a total makeover! In the meantime, if you need to reach out or connect, you can find me on <a target="_blank" href="https://www.linkedin.com/in/culinovic-a/">LinkedIn</a>.</h1>
+    </>
+  )
+}
+
+export default App
