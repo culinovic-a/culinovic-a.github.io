34b9eb3dc6b939b949339be80e316f2fa7c5a135
alex <aleksandra.c@badsistems.com>
2024-03-08 14:54:36 +0100

    change react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css'
+
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>Hey there! I'm giving my website a total makeover! In the meantime, if you need to reach out or connect, you can find me on <a target="_blank" href="https://www.linkedin.com/in/culinovic-a/">LinkedIn</a>.</h1>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
